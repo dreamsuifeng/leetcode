19a8c13020f2e75391c22588d5ef1768c6c2f3be
liyongchun <li948241252@163.com>
2017-06-08 12:52:14 +0800

    leetcode 316

--- 316.remove-duplicate-letters.java
+++ 316.remove-duplicate-letters.java
@@ -1,3 +1,4 @@
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Iterator;
 import java.util.PriorityQueue;
@@ -16,24 +17,37 @@ import java.util.Set;
  * Can you solve this problem? 🤔
  */
 public class Solution {
-    public String removeDuplicateLetters(String s) {
-        if (s==null || s.length()==0) return "";
-        PriorityQueue<Character> priQue=new PriorityQueue<>();
-        Set<Character> set=new HashSet<>();
-        char[] scha=s.toCharArray();
-        for (Character c:scha){
-            if (set.contains(c)){
-                continue;
-            }else{
-                set.add(c);
-                priQue.add(c);
-            }
+    private int findMIn(HashMap<Character, Integer> lastIndexMap){
+        if(lastIndexMap==null || lastIndexMap.isEmpty()) return -1;
+        int minlastIndex=Integer.MAX_VALUE;
+        for(int lastPos:lastIndexMap.values()){
+            minlastIndex=Math.min(minlastIndex,lastPos);
+        }
+        return minlastIndex;
+    }
+        public String removeDuplicateLetters(String s) {
+        if (s==null || s.length()==0) return s;
+        char[] schar=s.toCharArray();
+        HashMap<Character,Integer> lastIndexMap=new HashMap<>();
+        for (int i=0;i<schar.length;++i){
+            lastIndexMap.put(schar[i],i);
         }
         StringBuffer sb=new StringBuffer();
-        int n=priQue.size();
-        for (int i=0;i<n;++i){
-            sb.append(priQue.poll());
+        int n=lastIndexMap.size();
+        int begin=0,end=findMIn(lastIndexMap);
+        for(int i=0;i<n;++i){
+            char minchar='z'+1;
+            for(int k=begin;k<=end;++k){
+                if (lastIndexMap.containsKey(schar[k]) && schar[k]<minchar){
+                    minchar=schar[k];
+                    begin=k+1;
+                }
+            }
+            sb.append(minchar);
+            if (i==n-1) break;
+            lastIndexMap.remove(minchar);
+            if (schar[end]==minchar) end=findMIn(lastIndexMap);
         }
         return sb.toString();
-    }
+        }
 }
